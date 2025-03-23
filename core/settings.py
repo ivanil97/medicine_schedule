7bd46ee327f995524da908055f7059edc7d382fc
Ivan Ilin <ivaniliin@icloud.com>
2025-03-23 22:07:08 +0100

    minor fixes in .env and readme

--- core/settings.py
+++ core/settings.py
@@ -6,20 +6,14 @@ load_dotenv()
 
 
 class Settings:
-    secret_key: str = os.getenv(
-        'SECRET_KEY',
-        'phjsdviojr39480rusjdxio[09r2903mcx123d;x-94mcll;v')
-    access_token_expires_in_minutes: int = os.getenv(
-        'ACCESS_TOKEN_EXPIRES_IN_MINUTES', 60)
-
     db_user: str = os.getenv('DB_USER', 'admin')
     db_password: str = os.getenv('DB_PASS', '12345678')
     db_name: str = os.getenv('DB_NAME', 'med_schedule_api')
     db_host: str = os.getenv('DB_HOST', 'db')
     db_port: int = os.getenv('DB_PORT', 5432)
 
-    day_start: int = int(os.getenv('DAY_START', 8)) # начало дня в часах
-    day_end: int = int(os.getenv('DAY_END', 22)) # конец дня в часах
+    day_start: int = int(os.getenv('DAY_START', 8))  # начало дня в часах
+    day_end: int = int(os.getenv('DAY_END', 22))  # конец дня в часах
     next_taking_period: int = int(os.getenv('NEXT_TAKING_PERIOD', 3))  # ближайший период в часах
 
 
